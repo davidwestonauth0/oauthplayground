56860eebb0fecbd98fa5b5a50fdd532d48ae87da
David Weston <david.weston@auth0.com>
2022-06-10 16:28:42 +0100

    session debug

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
     secret: SESSION_SECRET,
-    resave: false,
+    resave: true,
     saveUninitialized: true,
   })
 );
